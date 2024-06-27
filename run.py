4512749370191dec28fe7efa24ef92ce3daf8404
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-06-27 22:51:50 +0100

    data and api connected

--- run.py
+++ run.py
@@ -12,7 +12,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    """
+    print("please enter details from last market")
+    print("data should be six numbers")
+    print("Example: 1, 2, 3\n")
 
-print(data)
+    data_str = input("Enter data here: ")
+    print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    try: 
+        if len(values) != 3:
+            raise ValueError(f"Stupid you provided {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again")
+
+get_sales_data()
