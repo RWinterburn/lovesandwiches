168cbfdc8b6bd6ed58905fd52bf2329ec6184571
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-06-30 12:12:16 +0100

    added validating data

--- run.py
+++ run.py
@@ -16,21 +16,34 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("please enter details from last market")
-    print("data should be six numbers")
-    print("Example: 1, 2, 3\n")
+    while True:
+        print("please enter details from last market")
+        print("data should be six numbers")
+        print("Example: 1, 2, 3\n")
 
-    data_str = input("Enter data here: ")
-    print(f"The data provided is {data_str}")
+        data_str = input("Enter data here: ")
+        print(f"The data provided is {data_str}")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+           print("Data is valid")
+           break
+        
+        
+        
+    
 
 def validate_data(values):
-    try: 
-        if len(values) != 3:
+    print(values)
+    try:
+        [int(value) for value in values] 
+        if len(values) != 6:
             raise ValueError(f"Stupid you provided {len(values)}")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again")
+        return False
+    
+    return True
 
-get_sales_data()
+get_sales_data() 
